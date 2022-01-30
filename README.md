f90bf4edca409abd60ae99ede71ae1764fd75bf9
Scarecrow0w <daniil6547@ya.ru>
2022-01-30 20:00:32 +0300

    task completed

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# demo
+# Разворачивание проекта:
+```
+git clone https://github.com/Scarecrow0w/demo.git
+npm install
+npm run dev
+```
+## Рекомендуемые версии npm и node:
+```
+node v16.13.2
+npm 8.1.2
+```
--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,20 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import ProductCard from './components/templates/ProductCard.vue';
 </script>
 
 <template>
-  <img class="mx-auto" alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <ProductCard 
+    :productDetail="{
+      descriptionText: 'Белое сухое вино, изготовленное из винограда сорта Мелонь де Бургонь, который произрастает на виноградниках в апелласьоне Мюскаде Севр э Мен. Сбор урожая проводится только по достижении ягодами оптимальной зрелости.',
+      ratingLink: { link: { url: '#', name: '23 отзыва' }, listLink: { url: '#', name: 'Характеристики' } },
+      h2Title: 'Chateau de la Turmeliere, Muscadet Sevre et Maine Sur Lie AOP, 2020',
+      h3Title: 'Тип вина',
+      code: '123456',
+      imageSrc: 'src/assets/bottle.png',
+      saleText: '-24%',
+      buttonText: 'Развернуть',
+      actionText: 'Купили 86 раз за сутки',
+      expertText: { lg: '98', sm: 'Wine 98' },
+      offerPrice: { type: 'sale', price: { previous: '2 990', current: '1 890' } } 
+    }" />
 </template>
-
-<style>
-
-</style>
